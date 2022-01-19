2396c81417cdd4b18f72e01fc86b04d7fd524755
Лидия <@DESKTOP-HL2BF8O>
2022-01-20 00:52:38 +0700

    Добавьте файлы проекта.

---
+++ CalcProfit/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace CalcProfit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBuy = new System.Windows.Forms.TextBox();
+            this.txtSell = new System.Windows.Forms.TextBox();
+            this.txtRent = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTaxes = new System.Windows.Forms.TextBox();
+            this.btnTaxes = new MaterialSkin.Controls.MaterialSwitch();
+            this.btnSteam = new MaterialSkin.Controls.MaterialButton();
+            this.materialButton1 = new MaterialSkin.Controls.MaterialButton();
+            this.materialButton2 = new MaterialSkin.Controls.MaterialButton();
+            this.materialButton3 = new MaterialSkin.Controls.MaterialButton();
+            this.materialButton4 = new MaterialSkin.Controls.MaterialButton();
+            this.SuspendLayout();
+            // 
+            // txtBuy
+            // 
+            this.txtBuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtBuy.Location = new System.Drawing.Point(177, 74);
+            this.txtBuy.Name = "txtBuy";
+            this.txtBuy.Size = new System.Drawing.Size(219, 38);
+            this.txtBuy.TabIndex = 0;
+            this.txtBuy.TextChanged += new System.EventHandler(this.txtBuy_TextChanged);
+            // 
+            // txtSell
+            // 
+            this.txtSell.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtSell.Location = new System.Drawing.Point(177, 118);
+            this.txtSell.Name = "txtSell";
+            this.txtSell.Size = new System.Drawing.Size(219, 38);
+            this.txtSell.TabIndex = 1;
+            this.txtSell.TextChanged += new System.EventHandler(this.txtSell_TextChanged);
+            // 
+            // txtRent
+            // 
+            this.txtRent.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtRent.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtRent.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtRent.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtRent.Location = new System.Drawing.Point(210, 191);
+            this.txtRent.Name = "txtRent";
+            this.txtRent.ReadOnly = true;
+            this.txtRent.Size = new System.Drawing.Size(219, 31);
+            this.txtRent.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(210, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Рентабельность";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(20, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Цена покупки";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(14, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Цена продажи";
+            // 
+            // txtTaxes
+            // 
+            this.txtTaxes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTaxes.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtTaxes.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtTaxes.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtTaxes.Location = new System.Drawing.Point(402, 118);
+            this.txtTaxes.Name = "txtTaxes";
+            this.txtTaxes.ReadOnly = true;
+            this.txtTaxes.Size = new System.Drawing.Size(198, 31);
+            this.txtTaxes.TabIndex = 6;
+            // 
+            // btnTaxes
+            // 
+            this.btnTaxes.AutoSize = true;
+            this.btnTaxes.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTaxes.Depth = 0;
+            this.btnTaxes.Location = new System.Drawing.Point(416, 86);
+            this.btnTaxes.Margin = new System.Windows.Forms.Padding(0);
+            this.btnTaxes.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.btnTaxes.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnTaxes.Name = "btnTaxes";
+            this.btnTaxes.Ripple = true;
+            this.btnTaxes.Size = new System.Drawing.Size(166, 37);
+            this.btnTaxes.TabIndex = 8;
+            this.btnTaxes.Text = "Комиссия 13%";
+            this.btnTaxes.UseVisualStyleBackColor = true;
+            this.btnTaxes.CheckedChanged += new System.EventHandler(this.btnTaxes_CheckedChanged);
+            // 
+            // btnSteam
+            // 
+            this.btnSteam.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnSteam.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnSteam.Depth = 0;
+            this.btnSteam.HighEmphasis = true;
+            this.btnSteam.Icon = null;
+            this.btnSteam.Location = new System.Drawing.Point(7, 236);
+            this.btnSteam.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnSteam.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnSteam.Name = "btnSteam";
+            this.btnSteam.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnSteam.Size = new System.Drawing.Size(70, 36);
+            this.btnSteam.TabIndex = 9;
+            this.btnSteam.Text = "Steam";
+            this.btnSteam.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnSteam.UseAccentColor = false;
+            this.btnSteam.UseVisualStyleBackColor = true;
+            this.btnSteam.Click += new System.EventHandler(this.btnSteam_Click);
+            // 
+            // materialButton1
+            // 
+            this.materialButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialButton1.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.materialButton1.Depth = 0;
+            this.materialButton1.HighEmphasis = true;
+            this.materialButton1.Icon = null;
+            this.materialButton1.Location = new System.Drawing.Point(85, 236);
+            this.materialButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialButton1.Name = "materialButton1";
+            this.materialButton1.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.materialButton1.Size = new System.Drawing.Size(89, 36);
+            this.materialButton1.TabIndex = 10;
+            this.materialButton1.Text = "Dmarket";
+            this.materialButton1.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.materialButton1.UseAccentColor = false;
+            this.materialButton1.UseVisualStyleBackColor = true;
+            this.materialButton1.Click += new System.EventHandler(this.materialButton1_Click);
+            // 
+            // materialButton2
+            // 
+            this.materialButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialButton2.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.materialButton2.Depth = 0;
+            this.materialButton2.HighEmphasis = true;
+            this.materialButton2.Icon = null;
+            this.materialButton2.Location = new System.Drawing.Point(182, 236);
+            this.materialButton2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialButton2.Name = "materialButton2";
+            this.materialButton2.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.materialButton2.Size = new System.Drawing.Size(64, 36);
+            this.materialButton2.TabIndex = 11;
+            this.materialButton2.Text = "BUFF";
+            this.materialButton2.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.materialButton2.UseAccentColor = false;
+            this.materialButton2.UseVisualStyleBackColor = true;
+            this.materialButton2.Click += new System.EventHandler(this.materialButton2_Click);
+            // 
+            // materialButton3
+            // 
+            this.materialButton3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialButton3.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.materialButton3.Depth = 0;
+            this.materialButton3.HighEmphasis = true;
+            this.materialButton3.Icon = null;
+            this.materialButton3.Location = new System.Drawing.Point(254, 236);
+            this.materialButton3.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialButton3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialButton3.Name = "materialButton3";
+            this.materialButton3.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.materialButton3.Size = new System.Drawing.Size(64, 36);
+            this.materialButton3.TabIndex = 12;
+            this.materialButton3.Text = "TM";
+            this.materialButton3.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.materialButton3.UseAccentColor = false;
+            this.materialButton3.UseVisualStyleBackColor = true;
+            this.materialButton3.Click += new System.EventHandler(this.materialButton3_Click);
+            // 
+            // materialButton4
+            // 
+            this.materialButton4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialButton4.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.materialButton4.Depth = 0;
+            this.materialButton4.HighEmphasis = true;
+            this.materialButton4.Icon = null;
+            this.materialButton4.Location = new System.Drawing.Point(506, 236);
+            this.materialButton4.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialButton4.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialButton4.Name = "materialButton4";
+            this.materialButton4.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.materialButton4.Size = new System.Drawing.Size(97, 36);
+            this.materialButton4.TabIndex = 13;
+            this.materialButton4.Text = "pecheNka";
+            this.materialButton4.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.materialButton4.UseAccentColor = false;
+            this.materialButton4.UseVisualStyleBackColor = true;
+            this.materialButton4.Click += new System.EventHandler(this.materialButton4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 281);
+            this.Controls.Add(this.materialButton4);
+            this.Controls.Add(this.materialButton3);
+            this.Controls.Add(this.materialButton2);
+            this.Controls.Add(this.materialButton1);
+            this.Controls.Add(this.btnSteam);
+            this.Controls.Add(this.btnTaxes);
+            this.Controls.Add(this.txtTaxes);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRent);
+            this.Controls.Add(this.txtSell);
+            this.Controls.Add(this.txtBuy);
+            this.FormStyle = MaterialSkin.Controls.MaterialForm.FormStyles.ActionBar_48;
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(257, 216);
+            this.Name = "Form1";
+            this.Opacity = 0.98D;
+            this.Padding = new System.Windows.Forms.Padding(3, 72, 3, 3);
+            this.Sizable = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Калькулятор рентабельности";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBuy;
+        private System.Windows.Forms.TextBox txtSell;
+        private System.Windows.Forms.TextBox txtRent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTaxes;
+        private MaterialSkin.Controls.MaterialSwitch btnTaxes;
+        private MaterialSkin.Controls.MaterialButton btnSteam;
+        private MaterialSkin.Controls.MaterialButton materialButton1;
+        private MaterialSkin.Controls.MaterialButton materialButton2;
+        private MaterialSkin.Controls.MaterialButton materialButton3;
+        private MaterialSkin.Controls.MaterialButton materialButton4;
+    }
+}
+
---
+++ CalcProfit/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+namespace CalcProfit
+{
+    public partial class Form1 : MaterialSkin.Controls.MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        void result()
+        {
+            try
+            {
+                double buy = Convert.ToDouble(txtBuy.Text),
+                        sell = 0.0,
+                        rent = 0.0;
+                if (btnTaxes.Checked) sell = 0.87 * Convert.ToDouble(txtSell.Text);
+                else sell = Convert.ToDouble(txtSell.Text);
+                txtTaxes.Text = sell.ToString();
+                rent = ((sell - buy) / buy) * 100.0;
+
+                txtRent.Text = Math.Round(rent, 3) + "%";
+            }
+            catch (Exception ex)
+            {
+                txtRent.Text = "null";
+            }
+        }
+
+        private void txtSell_TextChanged(object sender, EventArgs e)
+        {
+            result();
+        }
+
+        private void txtBuy_TextChanged(object sender, EventArgs e)
+        {
+            result();
+        }
+
+        private void btnTaxes_CheckedChanged(object sender, EventArgs e)
+        {
+            result();
+        }
+
+        private void btnSteam_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://steamcommunity.com/market/");
+        }
+
+        private void materialButton1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://dmarket.com/ru/ingame-items/item-list/csgo-skins");
+        }
+
+        private void materialButton2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://buff.163.com/market/csgo#tab=selling&page_num=1");
+        }
+
+        private void materialButton3_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://market.csgo.com");
+        }
+
+        private void materialButton4_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://vk.com/id219176419");
+        }
+    }
+}
